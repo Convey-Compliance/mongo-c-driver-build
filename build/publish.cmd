91be6ec3538e2d5008808caad56d9d84fd85e0b4
Kyle Moon <kmoon@convey.com>
2015-04-21 09:42:20 -0500

    add IF ERRORLEVEL 4 exit /B 1

--- build/publish.cmd
+++ build/publish.cmd
@@ -1,9 +1,15 @@
-### this file is a sample taken from mongo-c-driver-legacy-build. must be adapted to new driver ####
-
+ECHO ### this file is a sample taken from mongo-c-driver-legacy-build. must be adapted to new driver ####
 
 robocopy ..\bin\Win32\Debug\ %1\user\mongoc-dll\ mongo-client*.dll /ZB /X /TEE
+IF ERRORLEVEL 4 exit /B 1
+
 robocopy ..\bin\Win32\Release\ %1\user\mongoc-dll\ mongo-client*.dll /ZB /X /TEE
+IF ERRORLEVEL 4 exit /B 1
+
 robocopy ..\bin\x64\Debug\ %1\user\mongoc-dll\ mongo-client*.dll /ZB /X /TEE
+IF ERRORLEVEL 4 exit /B 1
+
 robocopy ..\bin\x64\Release\ %1\user\mongoc-dll\ mongo-client*.dll /ZB /X /TEE
+IF ERRORLEVEL 4 exit /B 1
 
 EXIT /B 0
