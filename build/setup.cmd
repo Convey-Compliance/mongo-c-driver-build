8c8600c55ac34e62eccc9a6e51a3bbcd74de6cb6
Jose Sebastian Battig <jsbattig@convey.com>
2015-08-20 22:39:42 -0500

    B-0 removed parameter from setup to make it simpler to run within Convey realm

--- build/setup.cmd
+++ build/setup.cmd
@@ -1,14 +1,14 @@
-robocopy %1\user\MongoDB\v3.1.2c ..\MongoDB mongod.exe /ZB /X /TEE
+robocopy \\conveydev.com\files\dev\sdlc-ci\development\user\MongoDB\v3.1.2c ..\MongoDB mongod.exe /ZB /X /TEE
 robocopy \\conveydev.com\files\repository\third-party-software-installers\Mongo\mongo-c-driver-ssl ..\__submodules\mongo-c-driver\tests /E /X /TEE
 
-robocopy %1\user\openssl\Win32\Debug\lib ..\bin\Win32\Debug /E /X /TEE
-robocopy %1\user\openssl\Win32\Release\lib ..\bin\Win32\Release /E /X /TEE
-robocopy %1\user\openssl\x64\Debug\lib ..\bin\x64\Debug /E /X /TEE
-robocopy %1\user\openssl\x64\Release\lib ..\bin\x64\Release /E /X /TEE
+robocopy \\conveydev.com\files\dev\sdlc-ci\development\user\openssl\Win32\Debug\lib ..\bin\Win32\Debug /E /X /TEE
+robocopy \\conveydev.com\files\dev\sdlc-ci\development\user\openssl\Win32\Release\lib ..\bin\Win32\Release /E /X /TEE
+robocopy \\conveydev.com\files\dev\sdlc-ci\development\user\openssl\x64\Debug\lib ..\bin\x64\Debug /E /X /TEE
+robocopy \\conveydev.com\files\dev\sdlc-ci\development\user\openssl\x64\Release\lib ..\bin\x64\Release /E /X /TEE
 
-robocopy %1\user\openssl\Win32\Debug\include ..\include\Win32\Debug /E /X /TEE
-robocopy %1\user\openssl\Win32\Release\include ..\include\Win32\Release /E /X /TEE
-robocopy %1\user\openssl\x64\Debug\include ..\include\x64\Debug /E /X /TEE
-robocopy %1\user\openssl\x64\Release\include ..\include\x64\Release /E /X /TEE
+robocopy \\conveydev.com\files\dev\sdlc-ci\development\user\openssl\Win32\Debug\include ..\include\Win32\Debug /E /X /TEE
+robocopy \\conveydev.com\files\dev\sdlc-ci\development\user\openssl\Win32\Release\include ..\include\Win32\Release /E /X /TEE
+robocopy \\conveydev.com\files\dev\sdlc-ci\development\user\openssl\x64\Debug\include ..\include\x64\Debug /E /X /TEE
+robocopy \\conveydev.com\files\dev\sdlc-ci\development\user\openssl\x64\Release\include ..\include\x64\Release /E /X /TEE
 
 EXIT /B 0
